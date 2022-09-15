519e562c869e8c682cc8ace11c99d6a7c0366b27
Yakup Yasin Kalafat <yasin@thundra.io>
2022-09-15 09:03:26 +0300

    PR comment refactors

--- sidekick-agent-api-data-redaction/src/main/java/com/runsidekick/agent/api/dataredaction/DataRedactionContext.java
+++ sidekick-agent-api-data-redaction/src/main/java/com/runsidekick/agent/api/dataredaction/DataRedactionContext.java
@@ -5,13 +5,11 @@ package com.runsidekick.agent.api.dataredaction;
  */
 public class DataRedactionContext {
 
-    private Class<?> clazz;
-    private String fileName;
-    private String className;
-    private int lineNo;
-    private String methodName;
-    private String logExpression;
-    private String logMessage;
+    private final Class<?> clazz;
+    private final String fileName;
+    private final String className;
+    private final int lineNo;
+    private final String methodName;
 
     public DataRedactionContext(Class<?> clazz, String fileName, String className, int lineNo, String methodName) {
         this.clazz = clazz;
@@ -25,55 +23,20 @@ public class DataRedactionContext {
         return clazz;
     }
 
-    public void setClazz(Class<?> clazz) {
-        this.clazz = clazz;
-    }
-
     public String getFileName() {
         return fileName;
-    }
-
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
     }
 
     public String getClassName() {
         return className;
     }
 
-    public void setClassName(String className) {
-        this.className = className;
-    }
-
     public int getLineNo() {
         return lineNo;
-    }
-
-    public void setLineNo(int lineNo) {
-        this.lineNo = lineNo;
     }
 
     public String getMethodName() {
         return methodName;
     }
 
-    public void setMethodName(String methodName) {
-        this.methodName = methodName;
-    }
-
-    public String getLogExpression() {
-        return logExpression;
-    }
-
-    public void setLogExpression(String logExpression) {
-        this.logExpression = logExpression;
-    }
-
-    public String getLogMessage() {
-        return logMessage;
-    }
-
-    public void setLogMessage(String logMessage) {
-        this.logMessage = logMessage;
-    }
 }
--- sidekick-agent-data-redaction/src/main/java/com/runsidekick/agent/dataredaction/DataRedactionHelper.java
+++ sidekick-agent-data-redaction/src/main/java/com/runsidekick/agent/dataredaction/DataRedactionHelper.java
@@ -5,6 +5,7 @@ import com.runsidekick.agent.api.dataredaction.SidekickDataRedactionAPI;
 import com.runsidekick.agent.core.logger.LoggerFactory;
 import com.runsidekick.agent.core.util.PropertyUtils;
 import com.runsidekick.agent.core.util.StringUtils;
+import com.runsidekick.agent.core.util.map.ConcurrentWeakMap;
 import org.slf4j.Logger;
 
 import java.util.Map;
@@ -14,23 +15,25 @@ import java.util.Map;
  */
 public final class DataRedactionHelper {
 
-    private DataRedactionHelper() {
-
-    }
-
     private static final Logger LOGGER = LoggerFactory.getLogger(DataRedactionHelper.class);
 
     private static final String DATA_REDACTION_IMPL_CLASS =
             PropertyUtils.getStringProperty("sidekick.agent.dataredactionimplclass");
 
+    private static final ConcurrentWeakMap<ClassLoader, SidekickDataRedactionAPI> sidekickDataRedactionAPICache =
+            new ConcurrentWeakMap();
+
+    private DataRedactionHelper() {}
+
     public static boolean shouldRedactVariable(DataRedactionContext dataRedactionContext, String fieldName) {
         if (!StringUtils.isNullOrEmpty(DATA_REDACTION_IMPL_CLASS)) {
             if (fieldName != null) {
                 try {
-                    SidekickDataRedactionAPI dataRedactionInstance = getDataRedactionInstance(dataRedactionContext);
+                    SidekickDataRedactionAPI dataRedactionInstance = getDataRedactionInstance(
+                            dataRedactionContext.getClazz().getClassLoader());
                     return dataRedactionInstance.shouldRedactVariable(dataRedactionContext, fieldName);
                 } catch (Exception ex) {
-                    LOGGER.error(String.format("Unable to redact variable", ex.getMessage()));
+                    LOGGER.error(String.format("Unable to redact variable", ex));
                 }
             }
         }
@@ -42,22 +45,36 @@ public final class DataRedactionHelper {
             String logMessage) {
         if (!StringUtils.isNullOrEmpty(DATA_REDACTION_IMPL_CLASS)) {
             try {
-                SidekickDataRedactionAPI dataRedactionInstance = getDataRedactionInstance(dataRedactionContext);
+                SidekickDataRedactionAPI dataRedactionInstance = getDataRedactionInstance(
+                        dataRedactionContext.getClazz().getClassLoader());
                 return dataRedactionInstance.redactLogMessage(dataRedactionContext, serializedVariables, logExpression,
                         logMessage);
             } catch (Exception ex) {
-                LOGGER.error(String.format("Unable to redact log message", ex.getMessage()));
+                LOGGER.error(String.format("Unable to redact log message", ex));
             }
         }
         return logMessage;
     }
 
-    private static SidekickDataRedactionAPI getDataRedactionInstance(DataRedactionContext dataRedactionContext)
+
+    private static SidekickDataRedactionAPI getDataRedactionInstance(ClassLoader classLoader) throws Exception {
+        SidekickDataRedactionAPI sidekickDataRedactionAPI = sidekickDataRedactionAPICache.get(classLoader);
+        if (sidekickDataRedactionAPI == null) {
+            sidekickDataRedactionAPI = createSidekickDataRedactionInstance(classLoader);
+            SidekickDataRedactionAPI existingSidekickDataRedactionAPI =
+                    sidekickDataRedactionAPICache.putIfAbsent(classLoader, sidekickDataRedactionAPI);
+            if (existingSidekickDataRedactionAPI != null) {
+                sidekickDataRedactionAPI = existingSidekickDataRedactionAPI;
+            }
+        }
+        return sidekickDataRedactionAPI;
+    }
+
+    private static SidekickDataRedactionAPI createSidekickDataRedactionInstance(ClassLoader classLoader)
             throws Exception {
         Class dataRedactionImplClazz;
-        if (dataRedactionContext != null) {
-            dataRedactionImplClazz = Class.forName(DATA_REDACTION_IMPL_CLASS, false,
-                    dataRedactionContext.getClazz().getClassLoader());
+        if (classLoader != null) {
+            dataRedactionImplClazz = Class.forName(DATA_REDACTION_IMPL_CLASS, false, classLoader);
         } else {
             dataRedactionImplClazz = Class.forName(DATA_REDACTION_IMPL_CLASS);
         }
