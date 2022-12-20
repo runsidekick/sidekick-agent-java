8bf3accfb523f506b87f3237a98a369b7b7edc20
Yakup Yasin Kalafat <yasin@thundra.io>
2022-12-20 09:17:24 +0300

    Classname Refactor

--- sidekick-agent-probe-tag/src/main/java/com/runsidekick/agent/probetag/handler/request/EnableProbeTagRequestHandler.java
+++ sidekick-agent-probe-tag/src/main/java/com/runsidekick/agent/probetag/handler/request/EnableProbeTagRequestHandler.java
@@ -1,37 +1,38 @@
-package com.runsidekick.agent.probetag.handler;
+package com.runsidekick.agent.probetag.handler.request;
 
 import com.runsidekick.agent.broker.BrokerManager;
 import com.runsidekick.agent.broker.handler.request.impl.BaseRequestHandler;
 import com.runsidekick.agent.logpoint.LogPointSupport;
 import com.runsidekick.agent.probetag.request.EnableProbeTagRequest;
-import com.runsidekick.agent.probetag.response.EnableTagResponse;
+import com.runsidekick.agent.probetag.response.EnableProbeTagResponse;
 import com.runsidekick.agent.tracepoint.TracePointSupport;
 
 /**
  * @author yasin.kalafat
  */
-public class EnableProbeTagRequestHandler extends BaseRequestHandler<EnableProbeTagRequest, EnableTagResponse> {
+public class EnableProbeTagRequestHandler extends BaseRequestHandler<EnableProbeTagRequest, EnableProbeTagResponse> {
 
     public static final String REQUEST_NAME = "EnableProbeTagRequest";
 
     public EnableProbeTagRequestHandler() {
-        super(REQUEST_NAME, EnableProbeTagRequest.class, EnableTagResponse.class);
+        super(REQUEST_NAME, EnableProbeTagRequest.class, EnableProbeTagResponse.class);
     }
 
     @Override
-    public EnableTagResponse handleRequest(EnableProbeTagRequest request) {
+    public EnableProbeTagResponse handleRequest(EnableProbeTagRequest request) {
         try {
+            System.out.println(request.toString());
             TracePointSupport.enableTag(request.getTag(), request.getClient());
             LogPointSupport.enableTag(request.getTag(), request.getClient());
             BrokerManager.publishApplicationStatus();
             if (request.getClient() != null) {
                 BrokerManager.publishApplicationStatus(request.getClient());
             }
-            return new EnableTagResponse().
+            return new EnableProbeTagResponse().
                     setRequestId(request.getId()).
                     setClient(request.getClient());
         } catch (Throwable error) {
-            return new EnableTagResponse().
+            return new EnableProbeTagResponse().
                     setRequestId(request.getId()).
                     setClient(request.getClient()).
                     setError(error);
