9774c3e7015dada76e49b30738910692f511d990
Serkan ÖZAL <serkan@thundra.io>
2023-01-31 17:24:06 +0300

    Add dynamic attach support (#16)

--- sidekick-agent-instrument/src/main/java/com/runsidekick/agent/instrument/Agent.java
+++ sidekick-agent-instrument/src/main/java/com/runsidekick/agent/instrument/Agent.java
@@ -2,15 +2,10 @@ package com.runsidekick.agent.instrument;
 
 import com.runsidekick.agent.core.initialize.EnvironmentInitializerManager;
 import com.runsidekick.agent.core.instance.InstanceDiscovery;
-import com.runsidekick.agent.core.util.ExceptionUtils;
 
-import java.io.FileInputStream;
-import java.io.IOException;
 import java.lang.instrument.Instrumentation;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
-import java.util.jar.JarFile;
 
 /**
  * Main class of java agent class to be invoked and passed {@link Instrumentation} instance.
@@ -23,18 +18,24 @@ public final class Agent {
     }
 
     public static void agentmain(String arguments, Instrumentation instrumentation) {
-        handleArguments(arguments, instrumentation);
+        handleArguments(arguments);
+
         InstrumentSupport.activate(instrumentation, arguments);
+
         EnvironmentInitializerManager.ensureInitialized();
+
         for (AgentAware agentAware : InstanceDiscovery.instancesOf(AgentAware.class)) {
             agentAware.onAgentStart(arguments, instrumentation);
         }
     }
 
     public static void premain(String arguments, Instrumentation instrumentation) {
-        handleArguments(arguments, instrumentation);
+        handleArguments(arguments);
+
         InstrumentSupport.activate(instrumentation, arguments);
+
         EnvironmentInitializerManager.ensureInitialized();
+
         for (AgentAware agentAware : InstanceDiscovery.instancesOf(AgentAware.class)) {
             agentAware.onAgentStart(arguments, instrumentation);
         }
@@ -44,7 +45,7 @@ public final class Agent {
         if (arguments == null) {
             return null;
         }
-        String[] argParts = arguments.split("\\s*,\\s*");
+        String[] argParts = arguments.split("\\s*;\\s*");
         Map<String, String> argMap = new HashMap<String, String>(argParts.length);
         for (String argPart : argParts) {
             argPart = argPart.trim();
@@ -63,55 +64,15 @@ public final class Agent {
         return argMap;
     }
 
-    private static void handleArguments(String arguments, Instrumentation instrumentation) {
+    private static void handleArguments(String arguments) {
         Map<String, String> argMap = parseArguments(arguments);
         if (argMap == null) {
             return;
         }
-        String argValue;
-        argValue = argMap.get("jarsToLoad");
-        if (argValue != null) {
-            handleJarsToLoad(argValue, instrumentation);
-        }
-        argValue = argMap.get("propFilesToLoad");
-        if (argValue != null) {
-            handlePropFilesToLoad(argValue);
-        }
-    }
-
-    private static void handleJarsToLoad(String argValue, Instrumentation instrumentation) {
-        String[] splittedJarsToLoad = argValue.split("\\s*;\\s*");
-        for (String jarToLoad : splittedJarsToLoad) {
-            jarToLoad = jarToLoad.trim();
-            if (jarToLoad.length() > 0) {
-                System.out.println("[SIDEKICK] Loading jar " + jarToLoad + " ...");
-                try {
-                    instrumentation.appendToSystemClassLoaderSearch(new JarFile(jarToLoad));
-                } catch (IOException e) {
-                    ExceptionUtils.sneakyThrow(e);
-                }
-            }
-        }
-    }
-
-    private static void handlePropFilesToLoad(String argValue) {
-        String[] splittedPropFilesToLoad = argValue.split("\\s*;\\s*");
-        for (String propFileToLoad : splittedPropFilesToLoad) {
-            propFileToLoad = propFileToLoad.trim();
-            if (propFileToLoad.length() > 0) {
-                System.out.println("[SIDEKICK] Loading properties from " + propFileToLoad + " ...");
-                Properties properties = new Properties();
-                try {
-                    properties.load(new FileInputStream(propFileToLoad));
-                    for (String propName : properties.stringPropertyNames()) {
-                        String propValue = properties.getProperty(propName);
-                        System.out.println("[SIDEKICK] Loading property "+ propName + ": " + propValue + " ...");
-                        System.setProperty(propName, propValue);
-                    }
-                } catch (IOException e) {
-                    ExceptionUtils.sneakyThrow(e);
-                }
-            }
+        for (Map.Entry<String, String> e : argMap.entrySet()) {
+            String argName = e.getKey();
+            String argValue = e.getValue();
+            System.setProperty(argName, argValue);
         }
     }
 
